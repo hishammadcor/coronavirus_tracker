4845544f2ae39aeeade9eefa2b7225e7b627691a
Hisham Madcor <hisham.madcor@ejust.edu.eg>
2022-06-22 00:06:39 +0200

    Update __init__.py

--- corona/__init__.py
+++ corona/__init__.py
@@ -1,9 +1,10 @@
+#__init__.py file for maintaining the blue print.
 from flask import Flask
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
-    app.secret_key = "rocdammahsih"
+    app.secret_key = "hishammadcor"
 
     from . import corona_virues
 
